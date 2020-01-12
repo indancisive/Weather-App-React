3fc663fd568fd3b9c4c70fa8a057f5a801f200c6
indancisive <47584841+indancisive@users.noreply.github.com>
2020-01-12 15:06:15 -0500

    pushing local files

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <h1>Weather Finder</h1>
+        <p>Get temperature, weather conditions, etc.</p>
+    </div>
+);
+
+export default Titles;
